9671a9575e8bea11e056bca38ef76faad4d64a88
Subhajit Sahu <wolfram77@gmail.com>
2017-10-15 15:36:21 +0530

    :tv: use server function

--- index.js
+++ index.js
@@ -3,28 +3,20 @@ const net = require('net');
 
 // 1. global variables
 const E = process.env;
-const UPGRADE_REQUEST = (
-  'GET '+E.URL+' HTTP/1.1\r\n'+
-  'Host: '+E.HOST+'\r\n'+
+const BUFFER_EMPTY = Buffer.alloc(0);
+const tokenFn = (opt) => (
+  'GET '+opt.url+' HTTP/1.1\r\n'+
+  'Host: '+opt.host+'\r\n'+
   'Upgrade: tcp\r\n'+
-  'Authorization: Basic '+E.TOKEN+'\r\n'+
+  'Authorization: Basic '+opt.token+'\r\n'+
   '\r\n'
 );
-const UPGRADE_RESPONSE = (
+const tokenResFn = () => (
   'HTTP/1.1 101 Switching Protocols\r\n'+
   'Upgrade: tcp\r\n'+
   'Connection: Upgrade\r\n'+
   '\r\n'
 );
-const BUFFER_EMPTY = Buffer.alloc(0);
-const TOKEN_LEN = Buffer.byteLength(E.TOKEN, 'utf8');
-const server = net.createServer();
-const members = new Map();
-const clients = new Set();
-server.listen(E.PORT);
-var con = 0;
-
-
 
 function buffersConcat(bufs) {
   // 1. concat buffers into one
@@ -35,7 +27,6 @@ function buffersConcat(bufs) {
 };
 
 function packetRead(bufs, size) {
-  console.log('packetRead', bufs, size);
   // 1. is packet available?
   if(size<4) return;
   if(bufs[0].length<4) buffersConcat(bufs);
@@ -48,12 +39,10 @@ function packetRead(bufs, size) {
   const body = buf.slice(4+4+hsz, psz);
   const head = JSON.parse(hst);
   bufs[0] = buf.slice(psz);
-  console.log('-> ', head, body, psz);
   return {head, body, 'size': psz};
 };
 
 function packetWrite(head, body) {
-  console.log('packetWrite', head, body);
   // 1. some defaults
   head = head||{};
   body = body||BUFFER_EMPTY;
@@ -69,83 +58,84 @@ function packetWrite(head, body) {
   return buf;
 };
 
-function memberWrite(id, head, body) {
-  console.log('memberWrite', id, head, body);
-  // 1. write packet to a member
-  const soc = members.get(id);
-  soc.write(clients.has(id)? packetWrite(head, body) : body);
-};
+const Server = function(port) {
+  const TOKEN = tokenFn(opt);
+  const TOKEN_RES = tokenResFn(opt);
+  const TOKEN_LEN = Buffer.byteLength(TOKEN, 'utf8');
+  const server = net.createServer();
+  const members = new Map();
+  const clients = new Set();
+  server.listen(port);
+  var ids = 0;
 
-function clientsWrite(head, body) {
-  console.log('clientsWrite', head, body);
-  // 1. write packet to all clients
-  const buf = packetWrite(head, body);
-  for(var id of clients)
-    members.get(id).write(buf);
-};
-
-server.on('connection', (soc) => {
-  console.log('connection');
-  // 1. connection data
-  const id = con++;
-  const bufs = [];
-  var size = 0, old = false;
-
-  function handleToken() {
-    console.log('handleToken');
-    // 1. if valid token, add client
-    const tok = bufs[0].toString();
-    if(!tok.startsWith(E.TOKEN) || !tok.startsWith(UPGRADE_REQUEST)) return;
-    if(tok.startsWith(UPGRADE_REQUEST)) soc.write(UPGRADE_RESPONSE);
-    console.log('token matched! adding client');
-    clientsWrite({'event': 'client', 'id': id});
-    bufs[0] = bufs[0].slice(TOKEN_LEN);
-    size -= TOKEN_LEN;
-    clients.add(id);
+  function memberWrite(id, head, body) {
+    // 1. write packet to a member
+    const soc = members.get(id);
+    soc.write(clients.has(id)? packetWrite(head, body) : body);
   };
 
-  function handleClient() {
-    console.log('handleClient');
-    // 1. write packets to members
-    var p = null;
-    while(p = packetRead(bufs, size)) {
-      memberWrite(p.id, {'event': 'data', 'id': id}, p.body);
-      size -= p.size;
-    }
+  function clientsWrite(head, body) {
+    // 1. write packet to all clients
+    const buf = packetWrite(head, body);
+    for(var id of clients)
+      members.get(id).write(buf);
   };
 
-  // 3. register member
-  members.set(id, soc);
-  clientsWrite({'event': 'connection', 'id': id});
-  // 4. handle events
-  soc.on('data', (buf) => {
-    console.log('data');
-    // a. update buffers
-    size += buf.length;
-    bufs.push(buf);
-    // b. handle actions
-    if(!old) handleToken();
-    if(clients.has(id)) handleClient();
-    else clientsWrite({'event': 'data', 'id': id}, buf);
-    old = true;
-  });
-  // 5. on close remove member
-  soc.on('close', () => {
-    console.log('close');
-    clients.delete(id);
-    members.delete(id);
-    clientsWrite({'event': 'close', 'id': id});
-  });
-  // 6. handle error
-  soc.on('error', (err) => {
-    console.log('error:connection');
-    console.error(err)
-  });
-});
+  server.on('connection', (soc) => {
+    // 1. connection data
+    const id = ids++;
+    const bufs = [];
+    var size = 0, old = false;
 
-server.on('error', (err) => {
-  console.log('error:server');
-  // 1. server must be closed on error
-  console.error(err);
-  server.close();
-});
+    function handleToken() {
+      // 1. verify token, if valid send response
+      if(!bufs[0].toString().startsWith(TOKEN)) return;
+      console.log(`Client ${id}.`);
+      clientsWrite({'event': 'client', 'id': id});
+      soc.write(TOKEN_RES);
+      bufs[0] = bufs[0].slice(TOKEN_LEN);
+      size -= TOKEN_LEN;
+      clients.add(id);
+    };
+
+    function handleClient() {
+      // 1. process client packets
+      var p = null;
+      while(p = packetRead(bufs, size)) {
+        memberWrite(p.id, {'event': 'data', 'id': id}, p.body);
+        size -= p.size;
+      }
+    };
+
+    // 2. register member
+    members.set(id, soc);
+    clientsWrite({'event': 'connection', 'id': id});
+    // 3. on data, process
+    soc.on('data', (buf) => {
+      // a. update buffers
+      size += buf.length;
+      bufs.push(buf);
+      // b. handle actions
+      if(!old) handleToken();
+      if(clients.has(id)) handleClient();
+      else clientsWrite({'event': 'data', 'id': id}, buf);
+      old = true;
+    });
+    // 4. on close, delete member and inform
+    soc.on('close', () => {
+      console.log(`Member ${id} close.`);
+      clients.delete(id);
+      members.delete(id);
+      clientsWrite({'event': 'close', 'id': id});
+    });
+    // 5. on error, report
+    soc.on('error', (err) => {
+      console.error(`Member ${id} error: `, err);
+    });
+  });
+  server.on('error', (err) => {
+    // 1. close server on error
+    console.error('Server error: ', err);
+    server.close();
+  });
+};
