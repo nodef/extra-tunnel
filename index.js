96c68860fa46d9bb828ec97f62aa330ee1c5d8b6
Subhajit Sahu <wolfram77@gmail.com>
2017-10-19 20:33:59 +0530

    :tv: setup defaults

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const url = require('url');
 const net = require('net');
 
 function Proxy(id, opt) {
+  // 1. setup defaults
+  opt = opt||{};
+  // 2. setup server
   const server = net.createServer();
   server.listen(opt.port||80);
 };
