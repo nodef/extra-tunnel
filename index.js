4d1a8cfe8477df23694ac0ac1677f8b07e121c79
Subhajit Sahu <wolfram77@gmail.com>
2017-10-19 21:40:29 +0530

    :tv: log close, error events

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ function Proxy(px, opt) {
     const id = idn++;
     sockets.set(id, soc);
     console.log(`${px}:${id} connected`);
-    // 1. if error, report to general
-    soc.on('error', (err) => {
-      console.error(`${px}:${id} error:`, err);
-    });
+    // 1. if closed or error, report to general
+    soc.on('close', () => console.log(`${px}:${id} closed`));
+    soc.on('error', (err) => console.error(`${px}:${id} error:`, err));
   });
   // 4. if closed, close all sockets
   proxy.on('close', () => {
