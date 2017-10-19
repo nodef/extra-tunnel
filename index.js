46d01720a29a7fd93caad0c969c73f69c88a1667
Subhajit Sahu <wolfram77@gmail.com>
2017-10-19 21:20:11 +0530

    :tv: lets say px = prefix

--- index.js
+++ index.js
@@ -2,27 +2,27 @@
 const url = require('url');
 const net = require('net');
 
-function Proxy(id, opt) {
+function Proxy(px, opt) {
   // 1. setup defaults
   opt = opt||{};
   // 2. setup server
   const server = net.createServer();
   const sockets = new Map();
   server.listen(opt.port||80);
-  var sidn = 0;
+  var idn = 0;
 
   // 3. ahhh, a new begining
   server.on('connection', (soc) => {
-    const sid = sidn++;
-    sockets.set(sid, soc);
-    console.log(`${id}:${sid} connected`);
+    const id = idn++;
+    sockets.set(id, soc);
+    console.log(`${px}:${id} connected`);
   });
   // 4. bad things happen sometimes
   server.on('close', () => {
-    console.log(`${id} closed`);
+    console.log(`${px} closed`);
   });
   server.on('error', (err) => {
-    console.error(`${id} error:`, err);
+    console.error(`${px} error:`, err);
     server.close();
   });
 };
