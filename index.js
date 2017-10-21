1b0382e8e372e071aeac6198308e5830510c72bd
Subhajit Sahu <wolfram77@gmail.com>
2017-10-22 04:26:37 +0530

    :tv: remove test logs

--- index.js
+++ index.js
@@ -172,9 +172,7 @@ function Proxy(px, opt) {
     });
     // e. data? write to client
     soc.on('data', (buf) => {
-      console.log('buf', buf);
       bsz = packetRead(bsz, bufs, buf, (on, set, tag, body) => {
-        console.log('on', on, 'set', set, 'tag', tag, 'body', body);
         if(clients.get(set)===chn) clientWrite(on, set, tag, body);
       });
     });
@@ -308,7 +306,6 @@ function Server(px, opt) {
     });
     // d. data? handle it
     soc.on('data', (buf) => {
-      console.log('on', 'd+', 'set', 0, 'id', id, 'buf', buf);
       proxy.write(packetWrite('d+', 0, id, buf));
     });
   };
@@ -336,7 +333,6 @@ function Server(px, opt) {
   proxy.on('data', (buf) => {
     // a. handle packets from proxy
     if(ath) return bsz = packetRead(bsz, bufs, buf, (on, set, tag, body) => {
-      console.log('on', on, 'set', set, 'tag', tag, 'body', body);
       const soc = sockets.get(tag);
       if(on==='c+') return socketAdd(tag);
       else if(!soc) return;
