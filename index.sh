7e51fd980fc996230b33cb6ce94feae034897496
Subhajit Sahu <wolfram77@gmail.com>
2017-12-30 20:31:34 +0530

    :snowman: use present script directory

--- index.sh
+++ index.sh
@@ -1,2 +1,9 @@
-dp0=$(dirname "$0")/
+psd() {
+  # present script directory
+  z="${BASH_SOURCE[0]}"
+  if [ -h "$z" ]; then z="$(readlink "$z")"; fi
+  cd "$(dirname "$0")" && cd "$(dirname "$z")" && pwd
+}
+
+dp0="$(psd)/"
 node "$dp0." "$@"
