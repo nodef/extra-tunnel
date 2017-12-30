29122643112b51faeced18e7d6d26eebb395f89d
Subhajit Sahu <wolfram77@gmail.com>
2017-12-30 20:29:03 +0530

    :snowman: update newline on linux

--- install.js
+++ install.js
@@ -3,6 +3,7 @@ const os = require('os');
 const cp = require('child_process');
 
 if(os.EOL==='\n') cp.execSync(
+  `tr -d '\r' <index.sh >index.cmd && `+
   'cp index.sh index.cmd && '+
   'chmod +x index.cmd'
 );
